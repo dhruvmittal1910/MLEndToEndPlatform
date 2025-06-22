4ce4e7cff9d68eaf9c1c201aa6437d5a0508ea5d
Dhruv Mittal <dhruvmittal1910@gmail.com>
2025-06-22 14:01:13 -0400

    solved errors + web app

--- app.py
+++ app.py
@@ -22,12 +22,26 @@ def predict_api():
     data=request.json['data']
     # do standardization using pickle file
     #  we need to convert data into array as done in our notebook
-    new_data=scaler.transform(np.nan_to_num(np.array(list(data.values())).reshape(1,-1)))
+
+    raw_values = [float(v) if v != "" else 0.0 for v in data.values()]
+# list(data.values())
+    new_data=scaler.transform(np.nan_to_num(np.array(raw_values).reshape(1,-1)))
 
     # now we can predict using our model
     output=regModel.predict(new_data)
-    print(output[0])
+    print("hello  " ,output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    # data=[float(x) for x in request.form.values()]
+    data = [float(x) if x != "" else 0.0 for x in request.form.values()]
+    print("data ",data)
+    final_input=np.nan_to_num(scaler.transform((np.array(data).reshape(1,-1))))
+    print("final inpiut s", final_input)
+    output=regModel.predict(final_input)[0]
+    print(output)
+    return render_template('home.html',prediction_text='Predicted house price is {}'.format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
